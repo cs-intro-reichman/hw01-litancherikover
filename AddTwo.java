fb79b42bc05ccdd7f7feee5c135cb6f9dc5314ba
litan cherikover <litan.cherikover@runi.ac.il>
2023-12-21 00:08:53 +0200

    new linear

--- AddTwo.java
+++ AddTwo.java
@@ -1,7 +1,8 @@
 /*
  * Adds two given integers and prints the result in a fancy way.
  */
-public class AddTwo {
+public class AddTwo 
+{
 	public static void main(String[] args) {
 		int First_number = Integer.parseInt(args[0]);
 		int Second_number = Integer.parseInt(args[1]);
@@ -9,7 +10,6 @@ public class AddTwo {
 		int sum = First_number + Second_number;
 
 		System.out.println(First_number + " + " + Second_number + " = " + sum);	
-
 	}
 
 }
