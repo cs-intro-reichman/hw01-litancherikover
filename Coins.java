baa3bdd858f93109fc4a333b683cd7b888215ba8
litan cherikover <litan.cherikover@runi.ac.il>
2023-12-20 17:09:18 +0200

    junk code for coins

--- Coins.java
+++ Coins.java
@@ -3,7 +3,15 @@
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
 public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
+	public static void main(String[] args) 
+	{
+		int Total_cents=56;
+		int quarters = Total_cents / 25;
+        int remainingCents = Total_cents % 25;
+
+        // Print the result
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Remaining cents: " + remainingCents);
+
 	}
 }
