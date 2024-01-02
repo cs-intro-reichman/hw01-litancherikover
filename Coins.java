7b16baf138eaf5cf2e2d245686525210dd8e4c11
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-02 21:07:10 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -6,10 +6,11 @@ public class Coins {
 	public static void main(String[] args) 
 	{
 		//here, our program gets the quantity of cents
-		int Total_cents = Integer.parseInt(args[0]);
+		// #feedback: in java we start variable names with a non-capital letters.
+		int total_cents = Integer.parseInt(args[0]);
 		//here, the program calculates the quarters and remaining cents
-		int quarters = Total_cents / 25;
-		int remaining = Total_cents % 25;
+		int quarters = total_cents / 25;
+		int remaining = total_cents % 25;
 		//here, our program prints the number of quarters and remaining cents
 		System.out.println("Use " + quarters + " quarters and " + remaining + " cents");
 	}
